3956cbc094a83007f73c767abeb0b98b972e0ac6
FilipRusiecki <fil201222@gmail.com>
2025-01-07 21:22:30 +0000

    added homepage updates

---
+++ web/src/components/ProjectsGrid/ProjectsGrid.jsx
@@ -0,0 +1,10 @@
+const ProjectsGrid = () => {
+  return (
+    <div>
+      <h2>{'ProjectsGrid'}</h2>
+      <p>{'Find me in ./web/src/components/ProjectsGrid/ProjectsGrid.jsx'}</p>
+    </div>
+  )
+}
+
+export default ProjectsGrid
--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,34 @@
-// import { Link, routes } from '@redwoodjs/router'
+import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+
+import AppBanner from 'src/components/AppBanner/AppBanner';
+
+
 
 const HomePage = () => {
   return (
     <>
-      <Metadata title="Home" description="Home page" />
+	  <div className="container mx-auto bg-black">
+			<AppBanner></AppBanner>
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
-      </p>
-      {/*
-           My default route is named `home`, link to me with:
-           `<Link to={routes.home()}>Home</Link>`
-        */}
+			{/* <ProjectsProvider>
+				<ProjectsGrid></ProjectsGrid>
+			</ProjectsProvider> */}
+
+			<div className="mt-8 sm:mt-10 flex justify-center">
+				<Link
+					to="/projects"
+					className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300"
+					aria-label="More Projects"
+				>
+					{/* <Button title="More Projects" /> */}
+				</Link>
+			</div>
+		</div>
     </>
   )
 }
 
 export default HomePage
+
