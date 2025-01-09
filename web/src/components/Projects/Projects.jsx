af2457a560e22309527bf1c4207c6a047e5f12a2
FilipRusiecki <fil201222@gmail.com>
2025-01-09 20:03:38 +0000

    added more projects

--- web/src/components/Projects/Projects.jsx
+++ web/src/components/Projects/Projects.jsx
@@ -1,10 +1,19 @@
 import { motion } from 'framer-motion'
 
+import frugo from './1.jpg'
 import zhill from './2.jpg'
 import Aipic from './ai.png'
+import chr from './chr1.jpg'
 import mcpic from './Dimension.png'
+import famas from './famas.png'
 import flowAlgo from './FlowAlgorithmWithHeatmap.png'
 import fuzzy from './fuzzy.png'
+import m4bg from './m4bg.png'
+import m4g from './m4g.png'
+import m4gw from './m4gw.png'
+import m4pb from './m4pb.png'
+import m4wg from './m4wg.png'
+import p250wg from './p250wg.png'
 import vrpic from './VR3.png'
 
 const Projects = () => {
@@ -156,6 +165,242 @@ const Projects = () => {
                 <span className="text-ternary-dark dark:text-ternary-light text-lg">
                   A gamemode map that is published on steam with over 21K
                   downloads and 4 star rating.
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Map for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3169731504"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img
+                src={frugo}
+                className="rounded-t-xl border-none"
+                alt="frugo map"
+              />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Map for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A map that is published on steam with over 16K downloads and 4
+                  star rating.
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Map for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=2303607674 "
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img src={chr} className="rounded-t-xl border-none" alt="chr" />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Map for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A map that is published on steam with over 10K downloads and 3
+                  star rating.
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img
+                src={famas}
+                className="rounded-t-xl border-none"
+                alt="famas"
+              />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img
+                src={p250wg}
+                className="rounded-t-xl border-none"
+                alt="p250wg"
+              />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img src={m4wg} className="rounded-t-xl border-none" alt="m4wg" />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img src={m4bg} className="rounded-t-xl border-none" alt="m4bg" />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img src={m4pb} className="rounded-t-xl border-none" alt="m4pb" />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img src={m4gw} className="rounded-t-xl border-none" alt="m4gw" />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img src={m4g} className="rounded-t-xl border-none" alt="Zm4g" />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a
+          aria-label="Gun Skin for Counter:Strike 2 Game"
+          href="https://steamcommunity.com/sharedfiles/filedetails/?id=3171183590	"
+        >
+          <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
+            <div>
+              <img
+                src={m4bg}
+                className="rounded-t-xl border-none"
+                alt="Zombie gamemode for Counter:Strike 2 Game"
+              />
+              <div className="px-4 py-6 text-center">
+                <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
+                  Gun Skin for Counter:Strike 2 Game
+                </p>
+                <span className="text-ternary-dark dark:text-ternary-light text-lg">
+                  A skin for a weapon inside the game published on steam
+                  workshop, Blender, Gimp/Photoshop and valve tools were used
                 </span>
               </div>
             </div>
